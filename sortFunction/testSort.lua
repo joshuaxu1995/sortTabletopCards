9fe0fcb29edc25c14eb185e5a8964027d723f6fe
joshuaxu1995 <joshxu95@gmail.com>
2020-05-03 21:21:02 -0500

    type in all card values

--- sortFunction/testSort.lua
+++ sortFunction/testSort.lua
@@ -33,6 +33,7 @@ cardTable = { ["87954f"] = {rank = 1, suit = "S"},
   ["7b1531"] = {rank = 1, suit = "D"},
   ["8896ce"] = {rank = 1, suit = "D"},
 
+
   ["9a59d7"] = {rank = 2, suit = "S"},
   ["5c2e94"] = {rank = 2, suit = "S"},
   ["54d371"] = {rank = 2, suit = "S"},
@@ -53,6 +54,7 @@ cardTable = { ["87954f"] = {rank = 1, suit = "S"},
   ["51825d"] = {rank = 2, suit = "D"},
   ["9e0978"] = {rank = 2, suit = "D"},
 
+
   ["ea4922"] = {rank = 3, suit = "S"},
   ["bc6fdf"] = {rank = 3, suit = "S"},
   ["9bc902"] = {rank = 3, suit = "S"},
@@ -71,7 +73,229 @@ cardTable = { ["87954f"] = {rank = 1, suit = "S"},
   ["365816"] = {rank = 3, suit = "D"},
   ["e42e04"] = {rank = 3, suit = "D"},
   ["e8efde"] = {rank = 3, suit = "D"},
-  ["ef31e3"] = {rank = 3, suit = "D"}
+  ["ef31e3"] = {rank = 3, suit = "D"},
+
+
+  ["d03d89"] = {rank = 4, suit = "S"},
+  ["0b0828"] = {rank = 4, suit = "S"},
+  ["c7a1f9"] = {rank = 4, suit = "S"},
+  ["43b9c1"] = {rank = 4, suit = "S"},
+
+  ["ec59d6"] = {rank = 4, suit = "C"},
+  ["80a7b5"] = {rank = 4, suit = "C"},
+  ["1e66dc"] = {rank = 4, suit = "C"},
+  ["78ad79"] = {rank = 4, suit = "C"},
+
+  ["5ff194"] = {rank = 4, suit = "H"},
+  ["6df6f4"] = {rank = 4, suit = "H"},
+  ["79706d"] = {rank = 4, suit = "H"},
+  ["6ddcfd"] = {rank = 4, suit = "H"},
+
+  ["e5e5e5"] = {rank = 4, suit = "D"},
+  ["88905d"] = {rank = 4, suit = "D"},
+  ["22d80e"] = {rank = 4, suit = "D"},
+  ["063800"] = {rank = 4, suit = "D"},
+
+
+  ["a2f60a"] = {rank = 5, suit = "S"},
+  ["05e6da"] = {rank = 5, suit = "S"},
+  ["6ed396"] = {rank = 5, suit = "S"},
+  ["89eba0"] = {rank = 5, suit = "S"},
+
+  ["62b5d6"] = {rank = 5, suit = "C"},
+  ["3f87a8"] = {rank = 5, suit = "C"},
+  ["78b55b"] = {rank = 5, suit = "C"},
+  ["6c21f3"] = {rank = 5, suit = "C"},
+
+  ["552e6b"] = {rank = 5, suit = "H"},
+  ["7940c6"] = {rank = 5, suit = "H"},
+  ["04721c"] = {rank = 5, suit = "H"},
+  ["321cd6"] = {rank = 5, suit = "H"},
+
+  ["9657c3"] = {rank = 5, suit = "D"},
+  ["a7071e"] = {rank = 5, suit = "D"},
+  ["5ae306"] = {rank = 5, suit = "D"},
+  ["2ce290"] = {rank = 5, suit = "D"}
+
+
+  ["fc534d"] = {rank = 6, suit = "S"},
+  ["cb7149"] = {rank = 6, suit = "S"},
+  ["8381bb"] = {rank = 6, suit = "S"},
+  ["09abb0"] = {rank = 6, suit = "S"},
+
+  ["214f87"] = {rank = 6, suit = "C"},
+  ["058114"] = {rank = 6, suit = "C"},
+  ["a6fd35"] = {rank = 6, suit = "C"},
+  ["dc0a44"] = {rank = 6, suit = "C"},
+
+  ["4e064b"] = {rank = 6, suit = "H"},
+  ["f5f9b9"] = {rank = 6, suit = "H"},
+  ["e2ad10"] = {rank = 6, suit = "H"},
+  ["790e94"] = {rank = 6, suit = "H"},
+
+  ["494072"] = {rank = 6, suit = "D"},
+  ["b30126"] = {rank = 6, suit = "D"},
+  ["20658f"] = {rank = 6, suit = "D"},
+  ["187534"] = {rank = 6, suit = "D"},
+
+
+  ["d3c35a"] = {rank = 7, suit = "S"},
+  ["ddd3b0"] = {rank = 7, suit = "S"},
+  ["37f7cc"] = {rank = 7, suit = "S"},
+  ["86aead"] = {rank = 7, suit = "S"},
+
+  ["7e28ef"] = {rank = 7, suit = "C"},
+  ["526710"] = {rank = 7, suit = "C"},
+  ["774e8a"] = {rank = 7, suit = "C"},
+  ["a8fad2"] = {rank = 7, suit = "C"},
+
+  ["9d87a1"] = {rank = 7, suit = "H"},
+  ["130c49"] = {rank = 7, suit = "H"},
+  ["6603f4"] = {rank = 7, suit = "H"},
+  ["03f2b5"] = {rank = 7, suit = "H"},
+
+  ["e3d211"] = {rank = 7, suit = "D"},
+  ["e23c5e"] = {rank = 7, suit = "D"},
+  ["76932d"] = {rank = 7, suit = "D"},
+  ["fa587d"] = {rank = 7, suit = "D"},
+
+
+  ["9f2d85"] = {rank = 8, suit = "S"},
+  ["d8cf96"] = {rank = 8, suit = "S"},
+  ["d9a385"] = {rank = 8, suit = "S"},
+  ["cac20c"] = {rank = 8, suit = "S"},
+
+  ["e39559"] = {rank = 8, suit = "C"},
+  ["6227e2"] = {rank = 8, suit = "C"},
+  ["49fbff"] = {rank = 8, suit = "C"},
+  ["5002b6"] = {rank = 8, suit = "C"},
+
+  ["d4bbd2"] = {rank = 8, suit = "H"},
+  ["4c36f7"] = {rank = 8, suit = "H"},
+  ["aa81e7"] = {rank = 8, suit = "H"},
+  ["3522d8"] = {rank = 8, suit = "H"},
+
+  ["045145"] = {rank = 8, suit = "D"},
+  ["750ba3"] = {rank = 8, suit = "D"},
+  ["5355aa"] = {rank = 8, suit = "D"},
+  ["555291"] = {rank = 8, suit = "D"},
+
+
+  ["69f1f8"] = {rank = 9, suit = "S"},
+  ["c822e9"] = {rank = 9, suit = "S"},
+  ["956f43"] = {rank = 9, suit = "S"},
+  ["4315cc"] = {rank = 9, suit = "S"},
+
+  ["5378bd"] = {rank = 9, suit = "C"},
+  ["b554b1"] = {rank = 9, suit = "C"},
+  ["2dfce8"] = {rank = 9, suit = "C"},
+  ["b4a4e1"] = {rank = 9, suit = "C"},
+
+  ["bf01e8"] = {rank = 9, suit = "H"},
+  ["3c55dd"] = {rank = 9, suit = "H"},
+  ["0abf64"] = {rank = 9, suit = "H"},
+  ["c8206e"] = {rank = 9, suit = "H"},
+
+  ["73d8ff"] = {rank = 9, suit = "D"},
+  ["d97b87"] = {rank = 9, suit = "D"},
+  ["e3a561"] = {rank = 9, suit = "D"},
+  ["19a0cf"] = {rank = 9, suit = "D"},
+
+
+  ["fea836"] = {rank = 10, suit = "S"},
+  ["481316"] = {rank = 10, suit = "S"},
+  ["90c049"] = {rank = 10, suit = "S"},
+  ["9197b2"] = {rank = 10, suit = "S"},
+
+  ["bc371a"] = {rank = 10, suit = "C"},
+  ["e24a93"] = {rank = 10, suit = "C"},
+  ["044e9c"] = {rank = 10, suit = "C"},
+  ["47d865"] = {rank = 10, suit = "C"},
+
+  ["105c2e"] = {rank = 10, suit = "H"},
+  ["0413e0"] = {rank = 10, suit = "H"},
+  ["0620ab"] = {rank = 10, suit = "H"},
+  ["65a0d3"] = {rank = 10, suit = "H"},
+
+  ["6e470a"] = {rank = 10, suit = "D"},
+  ["0c9571"] = {rank = 10, suit = "D"},
+  ["f0ea98"] = {rank = 10, suit = "D"},
+  ["29256f"] = {rank = 10, suit = "D"},
+
+
+  ["ae317a"] = {rank = 11, suit = "S"},
+  ["8f6ec9"] = {rank = 11, suit = "S"},
+  ["ef4b53"] = {rank = 11, suit = "S"},
+  ["d02ef5"] = {rank = 11, suit = "S"},
+
+  ["0dcdc9"] = {rank = 11, suit = "C"},
+  ["9bb57e"] = {rank = 11, suit = "C"},
+  ["679c2a"] = {rank = 11, suit = "C"},
+  ["8ed7ca"] = {rank = 11, suit = "C"},
+
+  ["3ff33b"] = {rank = 11, suit = "H"},
+  ["658cd0"] = {rank = 11, suit = "H"},
+  ["614adb"] = {rank = 11, suit = "H"},
+  ["7b403d"] = {rank = 11, suit = "H"},
+
+  ["43c089"] = {rank = 11, suit = "D"},
+  ["573855"] = {rank = 11, suit = "D"},
+  ["89eb21"] = {rank = 11, suit = "D"},
+  ["1a98f3"] = {rank = 11, suit = "D"},
+
+
+  ["7b011d"] = {rank = 12, suit = "S"},
+  ["e8df36"] = {rank = 12, suit = "S"},
+  ["1ba9c3"] = {rank = 12, suit = "S"},
+  ["633131"] = {rank = 12, suit = "S"},
+
+  ["03801a"] = {rank = 12, suit = "C"},
+  ["91f590"] = {rank = 12, suit = "C"},
+  ["bb863c"] = {rank = 12, suit = "C"},
+  ["dcb5c2"] = {rank = 12, suit = "C"},
+
+  ["d511b3"] = {rank = 12, suit = "H"},
+  ["89c2d2"] = {rank = 12, suit = "H"},
+  ["074a7c"] = {rank = 12, suit = "H"},
+  ["70b22e"] = {rank = 12, suit = "H"},
+
+  ["dee74b"] = {rank = 12, suit = "D"},
+  ["c3b692"] = {rank = 12, suit = "D"},
+  ["cc4912"] = {rank = 12, suit = "D"},
+  ["5a6bfa"] = {rank = 12, suit = "D"},
+
+
+  ["d40c29"] = {rank = 13, suit = "S"},
+  ["8646d6"] = {rank = 13, suit = "S"},
+  ["9edf5b"] = {rank = 13, suit = "S"},
+  ["5be43e"] = {rank = 13, suit = "S"},
+
+  ["e149dd"] = {rank = 13, suit = "C"},
+  ["19d992"] = {rank = 13, suit = "C"},
+  ["efe193"] = {rank = 13, suit = "C"},
+  ["63047e"] = {rank = 13, suit = "C"},
+
+  ["ee8c00"] = {rank = 13, suit = "H"},
+  ["d27c3f"] = {rank = 13, suit = "H"},
+  ["751576"] = {rank = 13, suit = "H"},
+  ["e693f7"] = {rank = 13, suit = "H"},
+
+  ["d39a75"] = {rank = 13, suit = "D"},
+  ["c4d6ff"] = {rank = 13, suit = "D"},
+  ["454a3b"] = {rank = 13, suit = "D"},
+  ["40c960"] = {rank = 13, suit = "D"},
+
+
+  ["9c5978"] = {rank = 14, suit = "S"},
+  ["735965"] = {rank = 14, suit = "S"},
+  ["d4ddb3"] = {rank = 14, suit = "S"},
+  ["cdf3c5"] = {rank = 14, suit = "S"},
+
+  ["7db4c9"] = {rank = 15, suit = "S"},
+  ["5c718e"] = {rank = 15, suit = "S"},
+  ["25551b"] = {rank = 15, suit = "S"},
+  ["be372e"] = {rank = 15, suit = "S"},
+
 }
 
 function sortObjects(t)
@@ -125,7 +349,6 @@ function showPlayerValues()
         print("The object here is " .. w.rank .. " and suit: " .. w.suit .. " and id: " .. w.id)
         print("The new position here is " .. logString(positions[i]))
         tempObject.setPosition(positions[j])
-
       end
     end
 end
